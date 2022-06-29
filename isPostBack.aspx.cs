03633a43aef6803e85740a96fe6838d7d24c8b19
msshaikh7388 <108425409+msshaikh7388@users.noreply.github.com>
2022-06-29 23:12:50 +0530

    Create isPostBack.aspx.cs

---
+++ isPostBack.aspx.cs
@@ -0,0 +1,14 @@
+protected void Page_Load(object sender, EventArgs e)
+{
+    LoadCityDropDownList();
+}
+public void LoadCityDropDownList()
+{
+    );
+    ddlCity.Items.Add("London");
+    ddlCity.Items.Add("Sydney");
+    ddlCity.Items.Add("Mumbai");
+}
+protected void Button1_Click(object sender, EventArgs e)
+{
+}
